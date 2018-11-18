ca5b24b7e520a5b51930f514ce57bfa7f3787852
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2018-11-18 13:01:26 -0600

    Create new person

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Add someone else</h1>
+    <div>
+      Name: <input type="text" v-model="newPersonName">
+      Bio: <input type="text" v-model="newBio">
+      <button v-on:click="createPerson()">Create a new famous person!</button>
+    </div>
+    <h1>My People</h1>
+    <div v-for="person in famousPeople">
+    <h2>{{ person.importantPeople }}</h2>
+    <h3>{{ person.bio }}</h3>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style>
+</style>
 
+<script>
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      newPersonName: "",
+      newBio: "",
+      famousPeople: [
+        {
+          id: 1,
+          importantPeople: "Ashley",
+          bio: "adjksahd laksdj alsjd sdkj laksjdieo dfjlsdjflaksdf ksdljeiw"
+        },
+        {
+          id: 1,
+          importantPeople: "Brittany",
+          bio: "adjksahd laksdj alsjd sdkj laksjdieo dfjlsdjflaksdf ksdljeiw"
+        },
+        {
+          id: 1,
+          importantPeople: "Melanie",
+          bio: "adjksahd laksdj alsjd sdkj laksjdieo dfjlsdjflaksdf ksdljeiw"
+        }
+      ]
+    };
+  },
+  created: function() {},
+  methods: {
+    createPerson: function() {
+      let newPerson = {
+        
+      }
+      this.newPersonName, this.newBio;
+    }
+  },
+  computed: {}
+};
 </script>
